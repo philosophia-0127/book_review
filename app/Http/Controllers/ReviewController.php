3693785428e2beccf58c5e5184a5517fb9f62f1f
konagaya <cresta522@gmail.com>
2021-07-23 17:05:52 +0900

    ソースレビュー

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -35,27 +35,8 @@ class ReviewController extends Controller
             'image' => 'mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
-        // if ($request->hasFile('image')) {
-        if ($request->image) {
-            $request->file('image')->store('/public/images');
-            $data = [
-                'user_id' => Auth::id(),
-                'title' => $post['title'],
-                'body' => $post['body'],
-                // $image = $request->image;
-                // $file_name = \Str::random(10) . '.' . $image->getClientOriginalExtension();
-                // $target_path = public_path('images/no_image.jpg');
-                // $image->move($target_path, $file_name);
-                ];
-        } else {
-            $data = [
-                'user_id' => Auth::id(),
-                'title' => $post['title'],
-                'body' => $post['body']
-            ];
-        }
-
-        Review::insert($data);
+        $Review = new Review();
+        $Review->fill($request->all())->save();
 
         return redirect('/index')
             ->with('flash_message', '投稿が完了しました！');
